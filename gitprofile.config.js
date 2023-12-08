74d85ba7e284d1ee4fa143dec648324a5b870fb5
Robert Chauncey <142944204+rmchauncey@users.noreply.github.com>
2023-12-08 15:10:03 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -130,6 +130,13 @@ const config = {
       imageUrl: 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L2pvYjcyNS0wMTIuanBn.jpg',
       link: 'https://docs.google.com/document/d/1u3ZAXjXHU9DgrlbRUadLONgieqyeFHO1/edit?usp=drive_link&ouid=108312881197280114839&rtpof=true&sd=true',
     },
+{
+      title: 'Splunk Activity',
+      description:
+        '',
+      imageUrl: 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L2pvYjcyNS0wMTIuanBn.jpg',
+      link: 'https://docs.google.com/document/d/1IB3lLAs5vhqrZWwcrgHW1SnLXWgL5ZW3/edit?usp=sharing&ouid=108312881197280114839&rtpof=true&sd=true',
+    },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
